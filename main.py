465bf65a14d164e939d8630721ce03964ed251f3
Kvidosamsa <159123624+Kvidosamsa@users.noreply.github.com>
2024-09-25 09:06:15 +0000

    verze 2

---
+++ main.py
@@ -0,0 +1,28 @@
+# 1. proměnná pro jméno
+jmeno = "Kvido"
+
+# 2. proměnná pro příjmení
+prijmeni = "Šamša"
+
+# 3. Výpis obou proměnných
+print(jmeno)
+print(prijmeni)
+
+# 4. Vytvoření vstupu pro uživatele
+vek = int(input("Zadejte svůj věk (pouze čísla): "))
+
+# 5. Výpis délky jména a příjmení
+
+print("délka jména : ",len(jmeno))
+
+print("délka příjmení :",len(prijmeni))
+
+# 6. Vytvoření proměnné s hodnotou 6
+cislo = 6
+
+# 7. Vytvoření cyklu který se opakuje 5x
+for i in range(5):
+  
+    cislo=cislo+3  # Přičítání 3
+# 8. Výsledek cyklu
+print("toto je výsledek cyklu : ", cislo)  
